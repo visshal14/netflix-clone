a9ade9290f1ddf56f0d22f8dd7e710e9dcf8a29a
visshal14 <vishalpal2912@gmail.com>
2023-01-07 23:51:48 +0530

    update files

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = "c388b7440361cafb242d9d44f7de507f";
+const API_KEY = "YOUR API_KEY";
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
@@ -7,7 +7,7 @@ const requests = {
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   fetchHorrorHovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres-10749`,
-  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with genres-99`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with genres=99`,
 };
 export default requests;
